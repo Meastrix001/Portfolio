795324df5a5970e7e4a16b6cf04eb5a9e7112fdd
Meastrix001 <nroofthooft2012@gmail.com>
2021-06-09 10:09:30 +0200

    first push

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+console.log(" JS running")
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+    eleventyConfig.addPassthroughCopy({
+        'src/main.css': 'assets/main.css',
+        'src/images': 'assets/img',
+        'src/js': 'assets/js',
+    });
+
+    return {
+        dir: {
+            input: 'views',
+            output: 'docs'
+        }
+    }
+}

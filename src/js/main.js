80b71b818d50e9d0d3286485198d8157bd655a8b
«Meastrix001» <Nroofthooft2012@gmail.com>
2021-10-17 01:24:33 +0200

    updated portfolio

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,34 @@
-const app = {
-  initialize () {
-    this.cacheElements();
-    this.registerEventListeners();
-    this.showToTopButton()
-  },
-  cacheElements () {
-    this.btnToTopElement = document.querySelector('.btn-to-top');
-    this.$menuOpen = document.querySelector('.btn-open')
-    this.$menuClose = document.querySelector('.btn-close')
-    this.$nav = document.querySelector('.nav')
-    this.$projectCard = document.querySelectorAll('.projects--card')
-    // this.navIcons = document.querySelector('.account')
-  },
-  registerEventListeners () {
-    if (this.$projectCard) {
-      console.log(this.$projectCard)
-      this.$projectCard.forEach(card => {
-        card.addEventListener('click', evt => {
-          console.log(evt.target.parentNode)
-          evt.target.parentNode.parentNode.classList.toggle('open')
-        })
+(() => {
+  const app = {
+    initialize () {
+      this.cacheElements();
+      this.registerEventListeners();
+    },
+    cacheElements () {
+      this.btnToTopElement = document.querySelector('.btn-to-top');
+      this.menuBtn = document.querySelector('.menu-btn');
+      this.nav = document.querySelector('.nav');
+      this.navExit = document.querySelector('.menu-btn-exit');
+    },
+    registerEventListeners () {
+      if (this.btnToTopElement !== null) {
+        this.btnToTopElement.addEventListener('click', (ev) => {
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          });
+        });
+      }
+      this.menuBtn.addEventListener("click", ev => {
+        this.nav.classList.toggle('open'),
+        this.navExit.classList.toggle('state')
       })
-    }
-    this.$menuOpen.addEventListener('click', evt => {
-      this.$menuOpen.classList.toggle('open')
-      this.$menuClose.classList.toggle('open')
-      this.$nav.classList.toggle('open')
-    })
-
-    this.$menuClose.addEventListener('click', evt => {
-      this.$menuOpen.classList.toggle('open')
-      this.$menuClose.classList.toggle('open')
-      this.$nav.classList.toggle('open')
-    })
-  },
-};
-
-app.initialize();
+      this.navExit.addEventListener("click", ev => {
+        this.nav.classList.toggle('open'),
+        this.navExit.classList.toggle('state')
+      })
+    },
+  };
+  app.initialize();
+})();

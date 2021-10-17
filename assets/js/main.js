c227aca261d30eb8b70c088483a392f71eb993cc
Meastrix001 <Meastrix001@users.noreply.github.com>
2021-10-17 12:54:31 +0000

    deploy: cdc82dfa2e38f7c0310a1ee9b1bdd2034d22f658

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,14 @@
     initialize () {
       this.cacheElements();
       this.registerEventListeners();
+      this.getAge();
     },
     cacheElements () {
       this.btnToTopElement = document.querySelector('.btn-to-top');
       this.menuBtn = document.querySelector('.menu-btn');
       this.nav = document.querySelector('.nav');
       this.navExit = document.querySelector('.menu-btn-exit');
+      this.age = document.getElementById('age')
     },
     registerEventListeners () {
       if (this.btnToTopElement !== null) {
@@ -29,6 +31,21 @@
         this.navExit.classList.toggle('state')
       })
     },
+
+    getAge() {
+
+      var userDateinput = "09/11/2000";  
+        // convert user input value into date object
+      var birthDate = new Date(userDateinput);
+       console.log(userDateinput);
+      
+      // get difference from current date;
+      var difference=Date.now() - birthDate.getTime(); 
+         
+      var  ageDate = new Date(difference); 
+      var calculatedAge=   Math.abs(ageDate.getUTCFullYear() - 1970);
+      return this.age.innerHTML = calculatedAge
+    }
   };
   app.initialize();
 })();

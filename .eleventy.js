9797f6cba827827c10975572ad490f6f3d606eb1
«Meastrix001» <Nroofthooft2012@gmail.com>
2021-10-17 12:00:50 +0200

    fixed responsivness bugs

--- .eleventy.js
+++ .eleventy.js
@@ -1,41 +1,14 @@
-const fs = require('fs');
-const pluginNavigation = require('@11ty/eleventy-navigation');
-
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+console.log(" JS running")
 module.exports = function (eleventyConfig) {
-  eleventyConfig.setUseGitIgnore(false);
-  eleventyConfig.addPlugin(pluginNavigation);
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  eleventyConfig.setDataDeepMerge(true);
+    eleventyConfig.addPassthroughCopy({
+        'src/main.css': 'assets/main.css',
+        'src/images': 'assets/img',
+        'src/js': 'assets/js',
+    });
 
-  eleventyConfig.addPassthroughCopy({
-    'src/main.css': 'assets/main.css',
-    'src/images': 'assets/img',
-    'src/js': 'assets/js',
-});
-
-  // Browsersync Overrides
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('docs/404.html');
-
-        browserSync.addMiddleware("*", (req, res) => {
-        });
-      }
-    }
-  });
-
-  // Filters
-  eleventyConfig.addFilter('artSpotlightFilter', function(collection) {
-      return collection.find(item => item.data.spotlight === true)
-  });
-
-  eleventyConfig.addFilter('getAmount', function(collection, amount) {
-    if (amount === undefined || amount === null) {
-      return collection;
-    }
-    return collection.slice(0, amount);
-  });
 
   return {
     dir: {

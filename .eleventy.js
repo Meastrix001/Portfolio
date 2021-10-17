36e3ca5c486b2ec3f8f34f5fdc59339dcdae8a83
«Meastrix001» <Nroofthooft2012@gmail.com>
2021-10-17 02:07:14 +0200

    deploy

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,56 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-console.log(" JS running")
+const fs = require('fs');
+const pluginNavigation = require('@11ty/eleventy-navigation');
+
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addPlugin(pluginNavigation);
 
-    eleventyConfig.addPassthroughCopy({
-        'src/main.css': 'assets/main.css',
-        'src/images': 'assets/img',
-        'src/js': 'assets/js',
-    });
+  eleventyConfig.setDataDeepMerge(true);
 
-    return {
-        dir: {
-            input: 'views',
-            output: 'docs'
-        }
+  eleventyConfig.addPassthroughCopy({ 'site/_static/css': 'static/css' });
+  eleventyConfig.addPassthroughCopy({ 'site/_static/images': 'static/images' });
+  eleventyConfig.addPassthroughCopy({ 'site/_static/js': 'static/js' });
+  eleventyConfig.addPassthroughCopy({ 'site/humans.txt': 'humans.txt' });
+
+  // Browsersync Overrides
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, browserSync) {
+        const content_404 = fs.readFileSync('docs/404.html');
+
+        browserSync.addMiddleware("*", (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
     }
+  });
+
+  // Filters
+  eleventyConfig.addFilter('artSpotlightFilter', function(collection) {
+      return collection.find(item => item.data.spotlight === true)
+  });
+
+  eleventyConfig.addFilter('getAmount', function(collection, amount) {
+    if (amount === undefined || amount === null) {
+      return collection;
+    }
+    return collection.slice(0, amount);
+  });
+
+  return {
+    dir: {
+      input: "views",
+      output: "docs",
+      includes: "includes",
+      data: "data",
+    },
+    pathPrefix: "/11tyio-boilerplate",
+    templateFormats: ["html", "liquid", "md", "njk"],
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
+  };
 }

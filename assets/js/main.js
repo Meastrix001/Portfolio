5b953f2ac74f5fba3a83378ba7005b2b0fd1714a
Meastrix001 <Meastrix001@users.noreply.github.com>
2021-11-21 19:45:33 +0000

    deploy: 3579b09e2bcf47fe7072cc11a806ce86ce80f053

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,11 @@
       this.nav = document.querySelector('.nav');
       this.navExit = document.querySelector('.menu-btn-exit');
       this.age = document.getElementById('age')
+      this.works = document.querySelectorAll(".work-tag")
+      this.clearBtn = document.querySelector('.clear-btn')
+      if (this.works) {
+        this.filterWorks()
+      }
       if(this.age) {
         this.getAge();
       }
@@ -41,6 +46,29 @@
       var  ageDate = new Date(difference); 
       var calculatedAge = Math.abs(ageDate.getUTCFullYear() - 1970);
       return this.age.innerHTML = calculatedAge
+    },
+    filterWorks() {
+            const searchURL = window.location.search
+            const searchForParam = new URLSearchParams(searchURL)
+            const getParam = searchForParam.get('tag')
+            if(getParam) {
+              this.clearBtn.style.display = "block"
+            }
+            this.works.forEach(work => {
+              if (getParam && work.dataset.id === getParam || getParam === null || getParam === undefined) {
+                work.closest(".work--cards--card").classList.add("shown")
+              } else if ( getParam && work.closest(".work--cards--card").classList.contains("shown") !== true) {
+                    work.closest(".work--cards--card").classList.add("hidden")
+
+                } else if(getParam && work.closest(".work--cards--card").classList.contains("shown") !== false) {
+
+                  work.closest(".work--cards--card").classList.add("shown")
+
+                } else {
+                  work.closest(".work--cards--card").classList.add("shown")
+                }
+              }
+            )
     }
   };
   app.initialize();
